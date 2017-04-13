3c5f4e9e14e560c3492c19c651e41a32a02a9bf8
Allison Bessett <allison_17@hotmail.com>
2017-04-12 17:14:55 -0700

    Final save. Working form

--- number-form.js
+++ number-form.js
@@ -1,76 +1,53 @@
-// TODO: add sum and average buttons
-//find sum and average functions 
-//add reset button
-//
-
-//function addEvent(el, event, callback) { // Check to see if using IE5-8
-//  if ('addEventListener' in el) {
-//    el.addEventListener(event, callback, false);
-//  } else {
-//    el['e' + event + callback] = callback;
-//    el[event + callback] = function () {
-//      el['e' + event + callback](window.event);
-//    };
-//    el.attachEvent('on' + event, el[event + callback]);
-//  }
-//} //End of IE check
-
-//function () {
-//var form = document.getElementById('numberForm'); // get form id 
-//var el, e; 
-//function target(e) {
-//  var el, e; 
-//  if (!e) { // check to see if IE supports event 
-//    e = window.event;
-//  }
-//  return e.target || e.srcElement;
-//}
-
-
-//addEvent (form, 'submit', function(e) {   //when form is submitted
-//  var number = document.getElementById('number').value;
-//  if (e.preventDefault) { //stops form from being sent
-//  e.preventDefault();
-//} else {
-//  e.returnValue = false;
-//} 
-//});
-
 var countMsg = document.createElement('h4'); // pg 285
 var countText = document.createTextNode('Count: ');
 countMsg.appendChild(countText);
 document.body.appendChild(countMsg);
 
+var sumMsg = document.createElement('h4');
+var sumText = document.createTextNode('Sum: ');
+sumMsg.appendChild(sumText);
+document.body.appendChild(sumMsg);
+
+var aveMsg = document.createElement('h4');
+var aveText = document.createTextNode('Average: ');
+aveMsg.appendChild(aveText);
+document.body.appendChild(aveMsg);
+
+var count = 0; 
+var sum = 0; 
+var avg = 0;
+
+
+function updateForm() {
+  countText.nodeValue = 'Count: ' + count;
+  sumText.nodeValue = 'Sum: ' + sum;
+  aveText.nodeValue = 'Average: ' + avg;
+}
+
 var submitButton = document.getElementById('submit');
 submitButton.addEventListener('click', function (e) {
   var number = document.getElementById('number').value;
-   countText.nodeValue += number;
+  count++;
+  sum += parseInt(number, 10);
+  avg = sum / count;
+  updateForm();
   if (e.preventDefault) { //stops form from being sent
-  e.preventDefault();
+    e.preventDefault();
   } else {
-  e.returnValue = false;
-  }                                                     
+    e.returnValue = false;
+  }                                                
 }, false);
-  
-countText.nodeValue += document.getElementById('number').value;
-console.log(countText);
 
 
-//var updateCount .addEventListener('click', addItem, false);
-//document.body.addEventListener('DOMNodeInserted', addCount, false);
-
-//var updateForm = document.createElement('div');
-//updateForm.setAttribute('id', 'form');
-//var countMsg = '<div id="count"><h4>Count: <h4></div>';
-// updateForm.innerHTML = countMsg;
-//
-//var sumMsg.innerHTML = '<div id="sum"><h4>Sum: <h4></div>';
-//
-//var aveMsg = '<div id="average"><h4>Average: <h4></div>';
-  
-
-//updateForm.innerHTML = countMsg;
-//updateForm.innerHTML = sumMsg;
-//updateForm.innerHTML = aveMsg;
-
-//document.body.appendChild(updateForm);
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', function (e) {
+  count = 0;
+  sum = 0;
+  avg = 0;
+  updateForm();
+  if (e.preventDefault) { //stops form from being sent
+    e.preventDefault();
+  } else {
+    e.returnValue = false;
+  }                                                
+}, false);
